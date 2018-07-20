8d478edda8ce2995726ef8d5c757ccd6b8d62d14
Emiliano Canedo <emilianocanedo@gmail.com>
2018-07-20 13:31:37 -0300

    num to string method

--- base_operations.py
+++ base_operations.py
@@ -3,14 +3,21 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
             'w', 'x', 'y', 'z']
 
 
+def num_to_string(num):
+    if num > 9:
+        return str(alphabet[num - 10])
+    else:
+        return str(num)
+
+
 def convert_from_base10(num, base, all_letters=False):
     new = []
-    while num > base:
+    while num >= base:
         new.insert(0, num % base)
         num = num // base
     else:
         new.insert(0, num)
     if all_letters:
-        return [alphabet[i] for i in new]
+        return ''.join([alphabet[i] for i in new])
     else:
-        return new
+        return ''.join([num_to_string(i) for i in new])
